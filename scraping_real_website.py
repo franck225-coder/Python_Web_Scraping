70e0139d268c1ff2a7354f07ca4f8216a3b95efa
franck-armand <13228201005@163.com>
2021-08-24 01:00:54 +0800

    access real website, scrape website info

--- scraping_real_website.py
+++ scraping_real_website.py
@@ -3,7 +3,27 @@
 # In you command line hit conda install requests
 
 import requests
+from bs4 import BeautifulSoup
 
-# Making sure the website we wanna scrape is working
-page_html = requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=")
-print(page_html)
+# Geting the website we wanna scrape
+page_url = 'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation='
+page_html = requests.get(page_url)
+page_html = page_html.text
+
+# create an instance of BeautifulSoup
+soup = BeautifulSoup(page_html, "lxml")
+
+# getting some attibutes for the job posted
+job_cards = soup.find_all("li", class_ = "clearfix job-bx wht-shd-bx")
+for job in job_cards:
+    # if the job was posted "few" days ago do not scrape
+    job_published_date = job.find("span", class_ = "sim-posted").span.text
+    if "few" in job_published_date:
+        company_name = job.find("h3", "joblist-comp-name").text.replace(" ","")
+        job_skill = job.find("span", class_ = "srp-skills").text.replace(" ","")
+
+        print(f'''
+            Company name: {company_name}
+            Required skill: {job_skill}
+            ''')
+        print("")
